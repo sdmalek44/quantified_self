100e77799b44f177f950af71ebff294da9e67269
Stephen Malek <malek.stephen44@gmail.com>
2018-10-02 14:21:06 -0600

    add test for 404 and 200 remove food from meal

--- app/controllers/api/v1/meals_controller.rb
+++ app/controllers/api/v1/meals_controller.rb
@@ -4,13 +4,18 @@ class Api::V1::MealsController < ApplicationController
   end
 
   def show
-    mp = MealPresenter.new(params[:id])
+    mp = MealPresenter.new(params[:meal_id])
     render json: mp.meal_foods, status: mp.status
   end
 
   def create
     mc = MealCreator.new(meal_params)
     render json: mc.message, status: mc.status
+  end
+
+  def destroy
+    mr = FoodRemover.new(meal_params)
+    render json: mr.message, status: mr.status
   end
 
   private

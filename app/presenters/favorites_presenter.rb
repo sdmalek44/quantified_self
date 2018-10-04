a113f9144224d98b4c9cb3b39004b6dc686dacd9
Stephen Malek <malek.stephen44@gmail.com>
2018-10-03 20:38:02 -0600

    change serialize food method to use pluck

--- app/presenters/favorites_presenter.rb
+++ app/presenters/favorites_presenter.rb
@@ -4,7 +4,7 @@ class FavoritesPresenter
     {
       name: food.name,
       calories: food.calories,
-      mealsWhenEaten: food.meals.map {|meal| meal.name }
+      mealsWhenEaten: food.meals.distinct.pluck(:name)
     }
   end
 
